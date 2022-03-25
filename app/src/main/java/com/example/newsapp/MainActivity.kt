bc20edd67ef8ef0fd5d2b3362c41fa7f5d2417e1
Aziza Helmy <aziza.helmy35@gmail.com>
2022-03-25 11:41:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.newsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
+
+
+
+
+    }
+}
